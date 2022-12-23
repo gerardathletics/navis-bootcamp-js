a3fae8d30a345c4db0d34a6f3f155548b66df577
Gerardathletics <gerardporras@gmail.com>
2022-12-23 16:45:41 +0100

    Small changes

--- 2-DOM/selectores.js
+++ 2-DOM/selectores.js
@@ -4,9 +4,10 @@ const listItems = document.querySelectorAll(".listItems")
 console.log(listItems)
 const listItemFirst =  document.querySelector
 
-// Create function to print the list items
-function printListItems(listItems) {
 
-        
-
-}
+// document.getElementById(): Accede a un elemento del DOM por su id.
+// document.querySelectorAll(): Accede a todos los elementos del DOM que coincidan con el selector CSS que le pasemos.
+// document.querySelector(): Accede al primer elemento del DOM que coincida con el selector CSS que le pasemos.
+// document.getElementsByName(): Accede a un elemento del DOM por su nombre.
+// document.getElementsByTagName(): Accede a un elemento del DOM por su etiqueta.
+// document.getElementsByClassName(): Accede a un elemento del DOM por su clase.
